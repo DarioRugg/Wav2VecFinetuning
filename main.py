bd0082c4c2bcd91e6402993fd1173099b7ee26ad
dario <ruggeri.1741637@studenti.uniroma1.it>
2021-03-17 15:20:20 +0100

    modified for run on server

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from scripts.train_test import train, test
 
 
 if __name__ == '__main__':
-    config = "effnet_home.json"
+    config = "effnet_server.json"
 
     train(conf_file=config)
     test(conf_file=config)
