9e96fce6b0aa51041301f07697118619cdcaf3bd
dario <ruggeri.1741637@studenti.uniroma1.it>
2021-03-21 19:00:48 +0100

    fixing config

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from scripts.train_test import train, test
 
 
 if __name__ == '__main__':
-    config = "effnet_server.json"
+    config = "wav2vec_server.json"
 
     train(conf_file=config)
     test(conf_file=config)
