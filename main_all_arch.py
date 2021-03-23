d2275c6b1d156ebef83241cf6c370527312abb6c
dario <ruggeri.1741637@studenti.uniroma1.it>
2021-03-23 21:30:42 +0100

    Make the repo ready for thesting the models

--- main_all_arch.py
+++ main_all_arch.py
@@ -2,7 +2,7 @@ from scripts.train_test import train, test
 
 
 if __name__ == '__main__':
-    config = ""
+    config = "wav2vec_train_all_server.json"
 
     train(conf_file=config)
     test(conf_file=config)
